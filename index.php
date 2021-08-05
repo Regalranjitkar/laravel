945dc1301d6132986d8a5afaa01420475941dca4
Regalranjitkar <regalranjitkar73425@outlook.com>
2021-08-05 12:14:57 +0545

    index page added

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                <img src="./img/blackhole.png" alt="" class="img img-fluid img-thumbnail">
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                <h4 class="text-center">User Info</h4>
+                <hr>
+                <div class="row">
+                    <div class="col-3"><strong>Name:</strong></div>
+                    <div class="col-9">Dipendra Ranjitkar</div>
+                </div>
+                <div class="row">
+                    <div class="col-3"><strong>Address:</strong></div>
+                    <div class="col-9">Purano Naikap</div>
+                </div>
+                <div class="row">
+                    <div class="col-3"><strong>E-mail:</strong></div>
+                    <div class="col-9">regalranjitkar7342@gmail.com</div>
+                </div>
+            </div>
+
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-bordered table-striped">
+                    <thead class="table-dark">
+                    	<th>S.N</th>
+                    	<th>Name</th>
+                    	<th>E-mail</th>
+                    	<th>Phone</th>
+                    	<th>Address</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1.</td>
+                            <td>User one</td>
+                            <td>userone@gmail.com</td>
+                            <td>9877451542</td>
+                            <td>Kathmandu</td>
+                        </tr>
+                        <tr>
+                            <td>1.</td>
+                            <td>User two</td>
+                            <td>usertwo@gmail.com</td>
+                            <td>9866325123</td>
+                            <td>Kathmandu</td>
+                        </tr>
+                        <tr>
+                            <td>1.</td>
+                            <td>User three</td>
+                            <td>userthree@gmail.com</td>
+                            <td>9865224157</td>
+                            <td>Kathmandu</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <hr>
+        <div class="row">Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Nemo atque quasi excepturi quod
+            illum autem rerum animi quia! Minus ipsa dolorem esse
+            eveniet ullam odit consequatur animi qui fugiat possimus.
+            Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Nemo atque quasi excepturi quod
+            illum autem rerum animi quia! Minus ipsa dolorem esse
+            eveniet ullam odit consequatur animi qui fugiat possimus.
+            Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Nemo atque quasi excepturi quod
+            illum autem rerum animi quia! Minus ipsa dolorem esse
+            eveniet ullam odit consequatur animi qui fugiat possimus.
+            Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Nemo atque quasi excepturi quod
+            illum autem rerum animi quia! Minus ipsa dolorem esse
+            eveniet ullam odit consequatur animi qui fugiat possimus.
+            Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Nemo atque quasi excepturi quod
+            illum autem rerum animi quia! Minus ipsa dolorem esse
+            eveniet ullam odit consequatur animi qui fugiat possimus.
+        </div>
+    </div>
+</body>
+</html>
